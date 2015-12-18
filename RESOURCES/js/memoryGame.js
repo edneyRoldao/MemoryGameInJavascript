4b15f5c71fe070d1b31f915a05bcf45449cf5197
edneyroldao <edneyroldao@yahoo.com.br>
2015-12-18 16:48:49 -0200

    third commit

--- RESOURCES/js/memoryGame.js
+++ RESOURCES/js/memoryGame.js
@@ -2,34 +2,25 @@ window.onload = function() {
 
 	var images = document.getElementsByTagName("img");
 
-	/*for(var i = 0; i < images.length; i++) {
-		var ran = Math.floor(Math.random()*5);
-		var imgPath = "../RESOURCES/img/theme";
-		var imgCompound = imgPath + ran + ".png";
-	}*/
-
-	var list = new Array();
-	for(var i = 0; i < 12; i++){
-		list[i] = Math.floor(Math.random()*20) + 1;
-	}
-
-	var thereIsEqNumber = true;
-
-	while(thereIsEqNumber) {
-		var r = Math.floor(Math.random()*20) + 1;
-		var exists = false;
-		for(var i = 0; i < list.length; i++){
-			if(list[i] == r) {
-				exists = true;
-				break;
-			}else {
-				list[i] = r;
-			}
-		}
-	}
-
-	alert(list);
-	alert(list.length)
-
+	var numberList = generateDistinctList();
+	alert(numberList);
 }
+
+function generateDistinctList() {
+	var list = [];
+	while(list.length < 12){
+  		var randomNumber = Math.ceil(Math.random()*19) + 1;
+  		var found = false;
+  		for(var i = 0; i < list.length; i++){
+			if(list[i] == randomNumber){
+				found = true;
+				break;
+			}
+  		}
+		 if(!found)
+		 	list[list.length] = randomNumber;
+	}
+	return list;
+}
+
 
