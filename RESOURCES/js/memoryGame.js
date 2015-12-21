943f9201978747a7eef8d8214353826a0ad9a43d
EdneyMAC <edneyroldao@gmail.com>
2015-12-20 23:48:31 -0200

    in this commit the game is working

--- RESOURCES/js/memoryGame.js
+++ RESOURCES/js/memoryGame.js
@@ -1,9 +1,11 @@
 window.onload = function() {
 	loadImagesToStartGame();
-	setTimeout(hideAllImages, 5000);
+	setTimeout(hideAllImages, 10000);
+	setTimeout(startTimer, 10000);
 }
 
-/* This method returns one array with randomic numbers, you can define size and interval of numbers */
+/* This method returns one array with randomic numbers, you can 
+	define size and interval of numbers */
 function generateDistinctList(arrayLength, randomInterval) {
 	var list = [];
 	while(list.length < arrayLength){
@@ -40,19 +42,26 @@ function buildImgPath(firstList, secondList) {
 }
 
 function loadImagesToStartGame() {
-	var images = document.getElementsByTagName("img");
+	var images = document.getElementsByName("imgInGame");
 	var randomList = generateDistinctList(12, 20);
 	var randomList2 = randomList;
 	var imagesName = buildImgPath(randomList, randomList2);
+
+	var shuffle = shuffleList(24, 24);
+	var imagesNameShuffled = [];
+	for(var i = 0; i < shuffle.length; i++){
+		imagesNameShuffled[i] = imagesName[shuffle[i]];
+	}
+
 	for(var i = 0; i < images.length; i++){
-		images[i].src = imagesName[i];
-		images[i].alt = imagesName[i];
+		images[i].src = imagesNameShuffled[i];
+		images[i].alt = imagesNameShuffled[i];
 	}
 
 }
 
 function hideAllImages() {
-	var images = document.getElementsByTagName("img");
+	var images = document.getElementsByName("imgInGame");
 	for(var i = 0; i < images.length; i++){
 		images[i].src = "../RESOURCES/img/shadow.png";
 	}
@@ -62,13 +71,12 @@ function hideImage(index) {
 
 	var idPersonSelected = returnIdPersonSelected(index);
 	
-	var array = document.getElementsByTagName("img");
+	var array = document.getElementsByName("imgInGame");
 
 	var aux = [];
 	for(var i = 0; i < array.length; i++){
 		aux[i] = returnIdPersonSelected(i);
 	}
-
 	var count = 0;
 	var indexAux = [];
 	for(var i = 0; i < aux.length; i++){
@@ -79,26 +87,30 @@ function hideImage(index) {
 	}
 	
 	if(count == 2) {
-		document.getElementsByTagName("img")[indexAux[0]].src =	document.getElementsByTagName("img")[indexAux[0]].alt;	
-		document.getElementsByTagName("img")[indexAux[1]].src =	document.getElementsByTagName("img")[indexAux[1]].alt;	
+		checkEndGame();
+
+		var altAux = document.getElementsByName("imgInGame")[indexAux[0]].alt;
+		var altAux2 = document.getElementsByName("imgInGame")[indexAux[1]].alt;
+		document.getElementsByName("imgInGame")[indexAux[0]].src =	altAux;	
+		document.getElementsByName("imgInGame")[indexAux[1]].src =	altAux2;
 	}else {
-		document.getElementsByTagName("img")[index].src = "../RESOURCES/img/shadow.png";
+		document.getElementsByName("imgInGame")[index].src = "../RESOURCES/img/shadow.png";
 	}
 
 }
 
 
 function imgSelectedByClick(index) {
-	var imgData = document.getElementsByTagName("img")[index];
+	var imgData = document.getElementsByName("imgInGame")[index];
 	imgData.src = imgData.alt;
 	setTimeout(function() {
 		hideImage(index);
-	}, 4000);
+	}, 1500);
 
 }
 
 function returnIdPersonSelected(index) {
-	var idPerson = document.getElementsByTagName("img")[index].src;
+	var idPerson = document.getElementsByName("imgInGame")[index].src;
 	idPerson = idPerson.replace('g/' , '@').replace('_' , '@').replace('.p' , '@');
 	var aux = idPerson.split('@');
 	idPerson = aux[1];
@@ -106,13 +118,81 @@ function returnIdPersonSelected(index) {
 }
 
 
+function shuffleList(arrayLength, randomInterval) {
+	var list = [];
+	while(list.length < arrayLength){
+  		var randomNumber = Math.ceil(Math.random() * randomInterval -1) + 1;
+  		var found = false;
+  		for(var i = 0; i < list.length; i++){
+			if(list[i] == randomNumber){
+				found = true;
+				break;
+			}
+  		}
+		 if(!found)
+		 	list[list.length] = randomNumber;
+	}
+
+	for(var i = 0; i < list.length; i++){
+		if(list[i] == list.length){
+			list[i] = 0;
+		}
+	}
+
+	return list;
+}
 
 
+function startTimer(){
+	var minutes = document.getElementById("minutes");
+	var seconds = document.getElementById("seconds");
+	var timeResulValue = document.getElementById("timerResulValue");
+	var totalSeconds = 0;
+
+	setInterval(countTime, 1000);
+
+	function countTime(){
+		++totalSeconds;
+		var secAux = runTime(totalSeconds%60);
+		var minAux = runTime(parseInt(totalSeconds/60));
+		seconds.innerHTML = secAux;
+		minutes.innerHTML = minAux;
+		timeResulValue.value = minAux + ":" + secAux;
+	}
+}
 
 
+function runTime(value){
+	var stringValue = value + "";
+	if(stringValue.length < 2){
+		return "0" + stringValue;
+	}else {
+		return stringValue;
+	}
+}
 
 
+function checkEndGame() {
+	var isEnd = endGame();
+	if(isEnd){
+		document.getElementById("timeResult").style.display = "block";
+		document.getElementById("timer").style.display = "none";
+		var totalTime = document.getElementById("timerResulValue").value;
+		document.getElementById("total").innerHTML = totalTime;
+	}
+}
 
+
+function endGame() {
+	var isGameOver = 0;
+	var arrayImg = document.getElementsByName("imgInGame");
+	for(var i = 0; i < arrayImg.length; i++){
+		var src = arrayImg[i].src;
+		if(src.indexOf("shadow") > -1)
+			isGameOver++;
+	}
+	return (isGameOver == 0) ? true : false;
+}
 
 
 
